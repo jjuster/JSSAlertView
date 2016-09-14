e251953e0570f772879909535c84c34ffa8ab58f
Josh Juster <juster@gmail.com>
2016-09-13 21:34:03 -0400

    Swift 2.3

--- Pod/Classes/JSSAlertView.swift
+++ Pod/Classes/JSSAlertView.swift
@@ -632,13 +632,13 @@ public extension UIImage {
 		UIGraphicsBeginImageContext(rect.size)
 		let context = UIGraphicsGetCurrentContext()
 		
-		CGContextSetFillColorWithColor(context, color.CGColor)
-		CGContextFillRect(context, rect)
+		CGContextSetFillColorWithColor(context!, color.CGColor)
+		CGContextFillRect(context!, rect)
 		
 		let image = UIGraphicsGetImageFromCurrentImageContext()
 		UIGraphicsEndImageContext()
 		
-		return image
+		return image!
 	}
 }
 
